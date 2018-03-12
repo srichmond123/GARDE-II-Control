72b1c1492693a6a7bc1dabf163aa814b9b85d5b4
Tyrone Tolbert <tjt280@nyu.edu>
2018-03-12 15:26:01 -0400

    update canvas, add click listener

---
+++ Assets/_Scenes/PanoScene/Scripts/EventListeners/TagClicked.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TagClicked : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // OnClick code goes here ...
+        // Currently, assuming the only thing being clicked is a tag
+        Debug.Log(this.name); // Name of the object
+    }
+}
