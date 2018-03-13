fe7716b1a61d7a66b3788e4ee0fad8d2358bb112
Tyrone Tolbert <tjt280@nyu.edu>
2018-03-13 18:43:33 -0400

    implement functionality for clicking on image

--- Assets/_Scenes/PanoScene/Scripts/EventListeners/ClickAction.cs
+++ Assets/_Scenes/PanoScene/Scripts/EventListeners/ClickAction.cs
@@ -5,6 +5,18 @@ using UnityEngine.EventSystems;
 
 public class ClickAction : MonoBehaviour, IPointerClickHandler
 {
+    StateManager state; // State of the application
+
+    private GameObject tagPrefab;
+    public Material tagMaterial;
+
+    public void Start()
+    {
+        state = GameObject.Find("Canvas").GetComponent<StateManager>();
+        tagPrefab = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        tagPrefab.transform.localScale = Vector3.zero;
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         // OnClick code goes here ...
@@ -21,6 +33,44 @@ public class ClickAction : MonoBehaviour, IPointerClickHandler
         else if (objectClicked.tag == "Image") // The image area was pressed, so here we cast a tag onto the sphere
         {
             Debug.Log("Image Clicked");
+            if (true) // TODO: Check if a tag is currently selected
+            {
+                Vector3 cursorPosition = state.getCursorPosition(); // Use the cursor position to cast a ray onto the sphere
+                Ray ray = Camera.main.ScreenPointToRay(cursorPosition);  // The ray that will be casted onto the sphere
+
+                // In the following two lines, since the sphere collider is outside the sphere
+                // We move the point of the ray well outside of the sphere, then invert the direction
+                // This way, we cast ray to the same point of the sphere, but from the outside rather than the inside
+                ray.origin = ray.GetPoint(100);
+                ray.direction = -ray.direction;
+
+                RaycastHit hit; // The raycast
+
+                Debug.DrawRay(ray.origin, ray.direction, Color.red, 5);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    GameObject newObject = Instantiate(tagPrefab, hit.point * 0.95f, Quaternion.identity); // Create the new object using the tagPrefab
+                    newObject.transform.LookAt(Vector3.zero); // Make it face the center of the sphere
+                    newObject.transform.localScale = new Vector3(0.2f, 0.1f, 0.00001f);
+                    newObject.name = "Some Name"; // CHANGE THIS LATER
+                    Debug.Log(hit.point);
+
+                    // ---- Below is old code used to create the tag whereever the click happened. It isn't being used now but may be useful later
+                    // --------------------------------------------------------------------------------------------------------------------------
+                    //GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(this.tag, hit.point * 0.95f, Quaternion.identity);
+                    //gameObject.transform.LookAt(Vector3.zero);
+                    //gameObject.name = "Tag " + this.tags.Count;
+                    //gameObject.transform.localScale = new Vector3(20f, 5f, 1f);
+                    //this.tag.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.Off;
+                    //gameObject.AddComponent(typeof(Tag));
+                    //gameObject.transform.parent = this.tagContainer.transform;
+                    //this.tags.Add(gameObject);
+                    //this.keycam.transform.position = Vector3.zero;
+                    //this.keycam.transform.LookAt(this.ray.point);
+                    //this.keycam.transform.position = Vector3.MoveTowards(this.keycam.transform.position, this.ray.point, Vector3.Distance(this.keycam.transform.position, this.ray.point) * 0.8f);
+                    //this.startTag(gameObject);
+                }
+            }
         }
     }
 }
--- Assets/_Scenes/PanoScene/Scripts/StateManager.cs
+++ Assets/_Scenes/PanoScene/Scripts/StateManager.cs
@@ -12,7 +12,11 @@ using UnityEngine;
 
 public class StateManager : MonoBehaviour {
 
-    private GameObject selected = null;
+    private GameObject selected = null; // The current tag being selected
+    private GameObject falcon; // The instance of the falcon controller
+    private GameObject falconCursor; // The cursor being manipulated by the falcon
+
+    private Vector3 cursorPosition; // The position of the cursor every frame
 
     public GameObject getSelected()
     {
@@ -23,4 +27,32 @@ public class StateManager : MonoBehaviour {
     {
         selected = g;
     }
+
+    public Vector3 getCursorPosition()
+    {
+        return this.cursorPosition;
+    }
+
+    private void Start()
+    {
+        this.falcon = GameObject.Find("Falcon");
+        if (this.falcon)
+        {
+            this.falconCursor = GameObject.Find("CursorSphere");
+        }
+    }
+
+    private void Update()
+    {
+        if (this.falcon) // Update the cursor position every frame
+        {
+            this.cursorPosition = Camera.main.WorldToScreenPoint(this.falconCursor.transform.position);
+        }
+        else
+        {
+            this.cursorPosition = Input.mousePosition;
+        }
+    }
+
+
 }
