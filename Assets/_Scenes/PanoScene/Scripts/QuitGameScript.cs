3e50bf66f16681276cc3aaf606a875b00c4bee72
Tyrone Tolbert <tjt280@nyu.edu>
2018-03-21 23:48:47 -0400

    update click listener
    QUITTING was printed on every click, so I updated the script to incorporate a click listener
    
    TODO: Test with falcon

--- Assets/_Scenes/PanoScene/Scripts/QuitGameScript.cs
+++ Assets/_Scenes/PanoScene/Scripts/QuitGameScript.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuitGameScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			Application.Quit();
-			Debug.Log ("QUITTING");
-		}
+	void TaskOnClick () {
+		Application.Quit();
+		Debug.Log ("QUITTING");
 	}
 }
