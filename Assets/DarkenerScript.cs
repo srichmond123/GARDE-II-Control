7ae1d2fd79b75606e5300c2c116f1d197b0f3821
Sam Richmond <sr4742@nyu.edu>
2018-04-06 16:32:22 -0400

    View cone and circle, and image fading to black added
    I added a panel that fades to black as the mouse scroll wheel increases to a certain measurement or decreases to a certain specific negative measurement. Also I put in a circle and view cone item that tells the user where they're rotated to in the image as well as how focused they are on it.

---
+++ Assets/DarkenerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DarkenerScript : MonoBehaviour {
+	// Use this for initialization
+	static float scrollPos = 0f;
+	static Image image;
+	static GameObject camera;
+	void Start () {
+		image = gameObject.GetComponent<Image> ();
+		camera = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseWheelDelta = Input.GetAxis ("Mouse ScrollWheel"); //This is the change in mousewheel position, not absolute position
+		scrollPos += mouseWheelDelta;
+		//Image blackens completely at above 0.33 scrollPos or below -0.33, this shouldn't be too hard to translate/modify over to the falcon:
+		image.color = new Color (0, 0, 0, Mathf.Abs(scrollPos) * 3);
+
+		//This part doesn't need to change at all:
+		ViewConeScript.modifyRotation (-camera.transform.localEulerAngles.y);
+
+		//With the falcon, the part "Mathf.Abs(1.0f + scrollPos)" can just be changed to "falcon.z / 2.0f + 1.0" (ranges from 0.5f to 1.5f scale)
+		ViewConeScript.modifyScale (Mathf.Abs(1.0f + scrollPos / 2.0f));
+	}
+}
