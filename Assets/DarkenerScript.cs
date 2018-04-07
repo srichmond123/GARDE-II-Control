8fc3d209af6ebbb055c78612fd0163fda58ad017
Tyrone Tolbert <tjt280@nyu.edu>
2018-04-07 14:34:37 -0400

    "lock" z axis, fade panel when falcon moves away
    Added a script that will apply a force opposite to the user's movement in the z axis. It doesn't lock the axis, but it's hard to move very far from it.
    
    The image fading now works with the falcon. I just replaced scrollPos with the falcon's position in the z axis when the falcon is being used.

--- Assets/DarkenerScript.cs
+++ Assets/DarkenerScript.cs
@@ -8,15 +8,33 @@ public class DarkenerScript : MonoBehaviour {
 	static float scrollPos = 0f;
 	static Image image;
 	static GameObject camera;
+
+    Vector3 falconPos;
+    GameObject falcon;
+
 	void Start () {
 		image = gameObject.GetComponent<Image> ();
 		camera = GameObject.Find("Main Camera");
+        if (falcon = GameObject.Find("Tip"))
+        {
+            falconPos = Vector3.zero;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float mouseWheelDelta = Input.GetAxis ("Mouse ScrollWheel"); //This is the change in mousewheel position, not absolute position
-		scrollPos += mouseWheelDelta;
+        if (!falcon)
+        {
+            float mouseWheelDelta = Input.GetAxis("Mouse ScrollWheel"); //This is the change in mousewheel position, not absolute position
+            scrollPos += mouseWheelDelta;
+        }
+        else
+        {
+            FalconUnity.getTipPosition(0, out falconPos);
+            scrollPos = falconPos.z * 0.5f;
+        }
+
+
 		//Image blackens completely at above 0.33 scrollPos or below -0.33, this shouldn't be too hard to translate/modify over to the falcon:
 		image.color = new Color (0, 0, 0, Mathf.Abs(scrollPos) * 3);
 
---
+++ Assets/PushToZ.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PushToZ : MonoBehaviour {
+
+    Vector3 pos;
+    public int k = 50;
+
+    void push()
+    {
+        FalconUnity.getTipPosition(0, out pos);
+        //if (Mathf.Abs(pos.z - wall) > 0.15f)
+        //    FalconUnity.setForceField(0, 500 * Vector3.back);
+        //FalconUnity.applyForce(0, 50*(pos.z-wall) * Vector3.back, 0.0001f);
+        //    FalconUnity.setForceField(0, 1000*(pos.z-wall) * Vector3.back);
+        //else
+        //    FalconUnity.setForceField(0, Vector3.zero);
+        FalconUnity.applyForce(0, k * pos.z * Vector3.back, 0.001f);
+    }
+
+    // Use this for initialization
+    void Start () {
+        AutoResetEvent a = new AutoResetEvent(false);
+        Timer timer = new Timer((e) => { push(); }, a, 1000, 1);
+    }
+}
