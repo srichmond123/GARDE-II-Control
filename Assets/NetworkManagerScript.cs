38dc97588fe26d508d90ab276c948dd337f62e6c
Sam Richmond <sr4742@nyu.edu>
2018-05-09 15:26:14 -0400

    HUD turns off

--- Assets/NetworkManagerScript.cs
+++ Assets/NetworkManagerScript.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class NetworkManagerScript : NetworkBehaviour {
-		public static bool hudOff = false;
-		NetworkManagerHUD hd;
-		// Use this for initialization
-		void Start () {
-				hd = FindObjectOfType<NetworkManagerHUD>();
-			}
-		
-		// Update is called once per frame
-		void Update () {
-				if (hudOff && hd.enabled) {
-						hd.enabled = false;
-					}
-			}
+public class NetworkManagerScript : MonoBehaviour {
+	public static bool hudOff = false;
+	NetworkManagerHUD hd;
+	// Use this for initialization
+	void Start () {
+		hd = FindObjectOfType<NetworkManagerHUD>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hudOff && hd.enabled) {
+			hd.enabled = false;
+		}
+	}
 }
