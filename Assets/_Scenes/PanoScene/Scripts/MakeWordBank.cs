70b6785a75374126322b65f4ddf0aa959ccc8fc3
Sam Richmond <sr4742@nyu.edu>
2018-05-09 15:44:37 -0400

    tags bug

--- Assets/_Scenes/PanoScene/Scripts/MakeWordBank.cs
+++ Assets/_Scenes/PanoScene/Scripts/MakeWordBank.cs
@@ -623,6 +623,7 @@ public class MakeWordBank : MonoBehaviour {
 						sequenceIndex = 0;
 						for (int tagsIndex = 0; tagsIndex < tags.Length; tagsIndex++) {
 							tags [tagsIndex].setText (wordBank [SEQUENCE [imageIndex, sequenceIndex]]);
+							tags [tagsIndex].text.color = Color.black;
 							sequenceIndex++;
 						}
 						if (inPracticeLevel) {
